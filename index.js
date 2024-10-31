8cd82868871c53ae9edfdecd7d2e0b97679e0a6e
SunJae <87426901+SSUNOWL@users.noreply.github.com>
2024-10-31 22:34:46 +0900

    Update index.js

--- index.js
+++ index.js
@@ -277,19 +277,11 @@ const GetVideoDetails = async (videoId) => {
     const playerData = await GetYoutubePlayerDetail(endpoint);
 
     const result = await page.initdata.contents.twoColumnWatchNextResults;
-    // const firstContent = await result.results.results.contents[0]
-    //   .videoPrimaryInfoRenderer;
     const secondContent = await result.results.results.contents[1]
       .videoSecondaryInfoRenderer;
     const res = await {
       id: playerData.videoId,
-      //title: firstContent.title.runs[0].text,
       thumbnail: playerData.thumbnail,
-      // isLive: firstContent.viewCount.videoViewCountRenderer.hasOwnProperty(
-      //   "isLive"
-      // )
-      //   ? firstContent.viewCount.videoViewCountRenderer.isLive
-      //   : false,
       channel:
         playerData.author ||
         secondContent.owner.videoOwnerRenderer.title.runs[0].text,
